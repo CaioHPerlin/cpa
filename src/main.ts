8f7328dd8b4ecf6920c70cd98404a101b610fa68
caiohperlin <caiohperlin@gmail.com>
2025-12-17 20:15:59 -0400

    feat: leverage config module on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { ConfigService } from "@nestjs/config";
+import { EnvironmentVariables } from "./config/environment-variables";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix("api");
+
+  const port = app.get(ConfigService<EnvironmentVariables>).get("PORT");
+  await app.listen(port);
 }
 bootstrap();
