9602a7541e1be2c3d216d2e15a25c63ba28d4ee3
caiohperlin <caiohperlin@gmail.com>
2025-12-19 19:58:06 -0400

    feat: prisma init

---
+++ src/db/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaClient } from "./generated/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { ConfigService } from "@nestjs/config";
+import { EnvironmentVariables } from "src/config/environment-variables";
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+	constructor(private configService: ConfigService<EnvironmentVariables>) {
+		const url = configService.get("DATABASE_URL");
+		const adapter = new PrismaBetterSqlite3({ url });
+		super({ adapter });
+	}
+}
